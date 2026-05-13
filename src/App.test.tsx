af13ac42f476c227c2f3ba869a39d4971da0d5be
Andrew Nickell <nickell.andrew@gmail.com>
2026-05-13 12:46:43 -0700

    Silence tile loading warning in App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+jest.mock('./assets/tiles', () => ({
+  ...jest.requireActual('./assets/tiles'),
+  useTileUrl: () => undefined,
+}));
+
 test('renders', () => {
   render(<App />);
   const linkElement = screen.getByText(/Lock Map/i);
